34dc16b6d73bd1ed3ca16c7144b3580e3b4c4341
Diana Selivanova <jasya555@mail.ru>
2020-02-01 14:44:23 +0700

    Initial commit

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,138 @@
+const path = require('path')
+const fs = require('fs')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+// Main const
+const PATHS = {
+  src: path.join(__dirname, '../src'),
+  dist: path.join(__dirname, '../dist'),
+  assets: 'assets/'
+}
+
+// Pages const for HtmlWebpackPlugin
+// const PAGES_DIR = PATHS.src
+const PAGES_DIR = `${PATHS.src}/pug/pages/`
+const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'))
+
+module.exports = {
+  // BASE config
+  externals: {
+    paths: PATHS
+  },
+  entry: {
+    app: PATHS.src,
+    // module: `${PATHS.src}/your-module.js`,
+  },
+  output: {
+    filename: `${PATHS.assets}js/[name].[hash].js`,
+    path: PATHS.dist,
+    publicPath: '/'
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          name: 'vendors',
+          test: /node_modules/,
+          chunks: 'all',
+          enforce: true
+        }
+      }
+    }
+  },
+
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      loader: 'pug-loader'
+    }, {
+      test: /\.js$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/'
+    }, {
+      test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]'
+      }
+    }, {
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+        }, {
+          loader: 'sass-loader',
+          options: { sourceMap: true }
+        }
+      ]
+    }, {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        MiniCssExtractPlugin.loader,
+        {
+          loader: 'css-loader',
+          options: { sourceMap: true }
+        }, {
+          loader: 'postcss-loader',
+          options: { sourceMap: true, config: { path: `./postcss.config.js` } }
+        }
+      ]
+    }]
+  },
+  resolve: {
+    alias: {
+      '~': PATHS.src,
+      'inputmask.dependencyLib': path.resolve(__dirname, '../node_modules/inputmask/lib/dependencyLibs/inputmask.dependencyLib.js'),
+      'inputmask': path.resolve(__dirname, '../node_modules/inputmask/dist/inputmask.js'),
+      'jquery.inputmask': path.resolve(__dirname, '../node_modules/inputmask/dist/jquery.inputmask.js'),
+      'inputmask.numeric.extensions': path.resolve(__dirname, '../node_modules/inputmask/lib/extensions/inputmask.numeric.extensions.js'),
+      'inputmask.date.extensions': path.resolve(__dirname, '../node_modules/inputmask/lib/extensions/inputmask.date.extensions.js'),
+
+      'jquery-ui-slider': path.resolve(__dirname, '../node_modules/jquery-ui-dist/jquery-ui.js'),
+
+      'air-datepicker': path.resolve(__dirname, '../node_modules/air-datepicker/dist/js/datepicker.min.js'),
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `${PATHS.assets}css/[name].[hash].css`,
+    }),
+    new CopyWebpackPlugin([
+      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
+      { from: `${PATHS.src}/static`, to: '' },
+    ]),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      'window.jQuery': 'jquery'
+    }),
+
+    // Automatic creation any html pages (Don't forget to RERUN dev server)
+    ...PAGES.map(page => new HtmlWebpackPlugin({
+      template: `${PAGES_DIR}/${page}`,
+      filename: `./${page.replace(/\.pug/,'.html')}`
+    }))
+  ],
+
+  //предупреждение об объеме файлов
+  performance: {
+    hints: process.env.NODE_ENV === 'production' ? "warning" : false
+  },
+}
+
---
+++ src/index.js
@@ -0,0 +1,19 @@
+// плагины
+//import 'inputmask'
+
+//скрипты
+
+//свои скрипты
+//import './blocks/FormElements/checkbox/checkbox.js'
+
+// SCSS
+import './assets/scss/main.scss'
+
+// CSS
+// import './assets/css/main.css'
+
+//Проверка работы JQUERY
+//jQuery(document).ready(function($) {
+//    $('body').css('background-color', 'orange');
+//    alert("sdfsdf");
+//});
